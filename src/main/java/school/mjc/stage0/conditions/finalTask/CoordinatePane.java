fa94be598a16d7012045624c640ca815e2eaa0e0
bakhridinova <bakhridinovaleyla@gmmail.com>
2023-01-20 03:12:26 +0500

    updated BitwiseValuesSwap.java, CoordinatePane.java, DaysInMonth.java, IntegerDivider.java, SeasonDeterminer.java, TriangleSidesValidator.java

--- src/main/java/school/mjc/stage0/conditions/finalTask/CoordinatePane.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/CoordinatePane.java
@@ -2,5 +2,22 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class CoordinatePane {
     public void printQuadrant(int x, int y) {
+        String quadrant;
+        if (x < 0) {
+            if (y < 0) {
+                quadrant = "third";
+            } else {
+                quadrant = "second";
+            }
+        } else if (x > 0) {
+            if (y < 0) {
+                quadrant = "fourth";
+            } else {
+                quadrant = "first";
+            }
+        } else {
+            quadrant = "zero";
+        }
+        System.out.println(quadrant);
     }
 }
